a10731d2a92502a8d86e2d60cc4a31525a77ea5b
cristhofer24 <you@example.com>
2025-03-27 09:27:08 -0500

    rutas ubicadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
+import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 
 export const routes: Routes = [
-{path: 'login', component: LoginComponent}
+{path: 'login', component: LoginComponent},
+{path:'home', component:HomeComponent},
+{path:'about',component:AboutComponent },
+{path:'contact',component:ContactComponent},
+{path:'**', redirectTo:'home'}
+
 
 ];
 
